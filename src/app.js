2f6097e1673c59e8cfeb382f7586fe84e0605751
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-07-29 08:52:40 -0300

    Adicionado incremento no número de likes de repositório

--- src/app.js
+++ src/app.js
@@ -61,7 +61,19 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' });
+  }
+
+  let likes = repositories[repositoryIndex]["likes"];
+
+  repositories[repositoryIndex]["likes"] = likes + 1;
+
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
